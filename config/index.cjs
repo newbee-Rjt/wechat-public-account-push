33a23f81b23275156852289d216208e539dd2467
newbee-Rjt <65074554+newbee-Rjt@users.noreply.github.com>
2023-10-16 13:42:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -68,7 +68,7 @@ SWITCH: {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ogPlX6z6sUlv6fyrfqIHjJXZfZ2o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'pGmWHoueCzHnqsI21FxEjkvjt2UmTB9XASREj5WYiA0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-19',
       festivals: [
@@ -96,7 +96,7 @@ SWITCH: {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: 'pGmWHoueCzHnqsI21FxEjkvjt2UmTB9XASREj5WYiA0',
 
   CALLBACK_USERS: [
     {
