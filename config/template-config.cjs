c21147e0288bbe01590a1c29734f8423f5965bb8
newbee-Rjt <65074554+newbee-Rjt@users.noreply.github.com>
2023-10-16 13:36:06 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,38 +2,39 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '嘿嘿秀秀，我又来给你发消息了嘞',
     desc: `
       **{{date.DATA}}**
+      今天是我们恋爱的第{{love_day.DATA}}天
       
-      下个休息日：{{holidaytts.DATA}}
+      所在的城市：{{city.DATA}}
       
-      ---
-      
-      城市：{{city.DATA}}
-      
-      天气：{{weather.DATA}}
+      今日天气：{{weather.DATA}}
       
       气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
       
       风向: {{wind_direction.DATA}}
-      
+
       风级: {{wind_scale.DATA}}
       
-      {{comprehensive_horoscope.DATA}}
+      每日一句(英文版)：{{note_en.DATA}}
       
-      ---
+      每日一句(中文版)：{{note_ch.DATA}}
       
-      今天是我们相识的第{{love_day.DATA}}天
+      再来一句情话吧hiahia:{{earthy_love_words.DATA}}
       
-      {{birthday_message.DATA}}
+      今日你的星座运势：
       
-      ---
+      爱情运势：{{love_horoscope.DATA}}
       
-      {{moment_copyrighting.DATA}}
+      事业学业：{{career_horoscope.DATA}}
       
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
-    `
+      财富运势：{{wealth_horoscope.DATA}}
+      
+      健康运势：{{healthy_horoscope.DATA}}
+      
+      综合运势：{{comprehensive_horoscope.DATA}}
+      `
   },
   {
     id: '0002',
